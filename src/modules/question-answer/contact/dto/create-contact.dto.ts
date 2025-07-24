9eb790330e151ba70ef08f1e9efc6eae80e65623
Asqaraliyev-Faxriddin <asqaraliyevfaxriddin2011@gmail.com>
2025-07-24 18:41:42 +0500

    Contact1

--- src/modules/question-answer/contact/dto/create-contact.dto.ts
+++ src/modules/question-answer/contact/dto/create-contact.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsPhoneNumber } from 'class-validator';
 
 export class CreateContactDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   fullName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsPhoneNumber('UZ')
   phone: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   message: string;
