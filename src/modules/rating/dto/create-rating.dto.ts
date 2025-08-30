fba28738aa49b910c8fe4fe86af3cdd7f77349a3
hacerovhacer <hacerovhacer253@gmail.com>
2025-08-30 12:35:33 +0500

    g

--- src/modules/rating/dto/create-rating.dto.ts
+++ src/modules/rating/dto/create-rating.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsUUID, IsInt, Min, Max, IsOptional, IsString } from 'class-validator';
 
 export class CreateCourseRatingDto {
+  @ApiProperty()
   @IsUUID()
   courseId: string;
 
+  @ApiProperty()
   @IsInt()
   @Min(1)
   @Max(5)
   rate: number;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   comment?: string;
